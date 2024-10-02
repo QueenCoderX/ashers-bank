78ea35920b687216c8b83d01a7096a79101b70e7
QueenCoderX <aze.lola14@gmail.com>
2024-10-02 12:44:40 +0100

    new file:   account.html 	new file:   css/account.css 	new file:   css/footer.css 	new file:   css/imgs/3.png 	new file:   css/imgs/5.png 	new file:   css/imgs/6.png 	new file:   css/imgs/7.png 	new file:   css/imgs/8.png 	new file:   css/imgs/Apple Icon (2).png 	new file:   css/imgs/Ashers Bank QR.png 	new file:   css/imgs/COCA.JPG 	new file:   css/imgs/Hero Background - Asher's (1).png 	new file:   css/imgs/IMG_4514.JPG 	new file:   css/imgs/IMG_4515.JPG 	new file:   css/imgs/IMG_4516.JPG 	new file:   css/imgs/IMG_4517.JPG 	new file:   css/imgs/IMG_4518.JPG 	new file:   css/imgs/IMG_4519.JPG 	new file:   css/imgs/IMG_4520.JPG 	new file:   css/imgs/IMG_4523.JPG 	new file:   css/imgs/IMG_4524.JPG 	new file:   css/imgs/IMG_4525.JPG 	new file:   css/imgs/IMG_4526.JPG 	new file:   css/imgs/IMG_4527.JPG 	new file:   css/imgs/IMG_4665.PNG 	new file:   css/imgs/IMG_E4523.JPG 	new file:   css/imgs/IMG_E4524.JPG 	new file:   css/imgs/IMG_E4526.JPG 	new file:   css/imgs/KFC.PNG 	new file:   css/imgs/Mobile Banking App - Ashers Bank.png 	new file:   css/imgs/Mobile Design - ASHERS APP (1).png 	new file:   css/imgs/PEPSI.WEBP 	new file:   css/imgs/Play Store Icon (2).png 	new file:   css/imgs/Screenshot 2024-10-02 081138.png 	new file:   css/imgs/Slot Logo.png 	new file:   css/imgs/The Place.png 	new file:   css/imgs/adobe-express-qr-code (1).png 	new file:   css/imgs/bg-removed.PNG 	new file:   css/imgs/glovo logo.png 	new file:   css/imgs/qr-code.png 	new file:   css/login.css 	new file:   css/registeration.css 	modified:   css/style.css 	new file:   footer.html 	new file:   icons/3.svg 	new file:   icons/4_1.svg 	new file:   icons/5_1.svg 	new file:   icons/6.svg 	new file:   icons/7.svg 	new file:   icons/8_1.svg 	new file:   icons/J7Zzby01.svg 	new file:   icons/NPyoD201 (1).svg 	new file:   icons/account_circle_32dp_E8EAED_FILL0_wght400_GRAD0_opsz40.svg 	new file:   icons/arrow_back_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/arrow_drop_down_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/arrow_drop_up_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/arrow_forward_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/award_star_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/close_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/dashboard_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/diversity_2_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/icons8-facebook (1).svg 	new file:   icons/icons8-instagram.svg 	new file:   icons/icons8-linkedin.svg 	new file:   icons/icons8-twitter.svg 	new file:   icons/icons8-twitterx.svg 	new file:   icons/key_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/keyboard_arrow_down_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/keyboard_arrow_up_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/ktanGv01.svg 	new file:   icons/lock_32dp_E8EAED_FILL0_wght400_GRAD0_opsz40.svg 	new file:   icons/menu_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/more_vert_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	new file:   icons/person_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg 	modified:   index.html 	new file:   javascript/account.js 	new file:   javascript/login.js 	modified:   javascript/main.js 	new file:   javascript/registration.js 	new file:   login.html 	new file:   node_modules/.package-lock.json 	new file:   package-lock.json 	new file:   package.json 	new file:   registration.html

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,77 @@
+/* Hamburger Menu Toggle Function */
 
-const menuToggle = document.getElementById("menu");
+
+const navLinks = document.querySelector('.navLinks');
+const navItems = document.querySelectorAll('.navItems');
+const toggleBtn = document.querySelector('.toggleBtn')
+const menuIcon = document.querySelector('.menuIcon');
+const closeIcon = document.querySelector('.closeIcon');
+
+
+function toggleMenu(){
+    console.log('Toggle here bitch');
+    if (navLinks.classList.contains("showMenu")){
+      navLinks.classList.remove("showMenu");
+      closeIcon.style.display = "none";
+      menuIcon.style.display = "block";
+      setTimeout(function(){
+        navLinks.style.transform = "translateY(-100%)";
+      }, 200);
+    }
+    else {
+      navLinks.classList.add("showMenu");
+      console.log('Debug')
+      closeIcon.style.display = "block";
+      menuIcon.style.display = "none";
+      setTimeout(function(){
+        navLinks.style.transform = "translateY(0)";
+      }, 200);
+    }
+  }
+
+toggleBtn.addEventListener("click", toggleMenu);
+
+navItems.forEach (
+    function (navItem) {
+        navItem.addEventListener("click", toggleMenu);
+    }
+)
+
+//main script//
+var swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  spaceBetween: 1,
+  freeMode: true,
+  touchEventsTarget: 'container',
+  width: 40,
+  margin: 5,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 315,
+    },
+  }
+});
+
+
+
+// var indexValue = 0;
+// const container = document.getElementsByClassName("container");
+// function slideShow(){
+//   setTimeout(slideShow, 2500);
+
+//   var x;
+
+//   for (x = 0; x < container.length; x++){
+//     container[x].style.display = "none";
+//   }
+//   indexValue++
+//   if(indexValue > container.length){indexValue = 1}
+//   container[indexValue  -1].style.display = "block";
+// }
+// slideShow();
